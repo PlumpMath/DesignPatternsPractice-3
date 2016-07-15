2272ebff0cac5d1c95a3c699cd5533d197fe42f4
Ash Kumar <kumar.ashwin@outlook.com>
2016-07-15 18:37:44 -0400

    Added practice code for basic proxy pattern

---
+++ Proxy/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proxy proxy = new Proxy();
+            proxy.Request();
+
+            Console.Read();
+        }
+    }
+
+    class Proxy : ISubject
+    {
+        private Subject _subject;
+
+        public void Request()
+        {
+            Console.WriteLine($"This method is being called on : {this.ToString()}");
+
+            //create a lazy loader for the Subject
+            if (_subject == null)
+                _subject = new Subject();
+
+            Console.WriteLine("  |");
+            Console.Write("  ---");
+
+            _subject.Request();
+        }
+
+        public override string ToString()
+        {
+            return $"Proxy";
+        }
+    }
+
+    class Subject : ISubject
+    {
+        public void Request()
+        {
+            Console.WriteLine($"This method is being called on : {this.ToString()}");
+        }
+
+        public override string ToString()
+        {
+            return $"Subject";
+        }
+    }
+
+    internal interface ISubject
+    {
+        void Request();
+    }
+
+    
+}
